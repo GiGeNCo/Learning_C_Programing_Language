7b97be3e9ff4cb43d8997da5065d3af9254aa95b
GiGeNCo <gigalfc@gmail.com>
2018-12-31 23:46:11 +0400

    add timeconverter

--- Clion/TimeConverter/main.c
+++ Clion/TimeConverter/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Input Seconds\n");
+    int seconds, h,m,s;
+    scanf("%d",&seconds);
+
+    h = seconds / 3600;
+    seconds = seconds % 3600;
+    m = seconds / 60;
+    seconds = seconds % 60;
+    s = seconds;
+
+    printf("HH:MM:SS - %d:%d:%d",h,m,s);
+
+
+
     return 0;
 }
